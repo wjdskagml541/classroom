0cf0f45d1f86a3875f68fe292eca4c686ff36787
wjdskagml541 <tnfltnfl541@gmail.com>
2020-07-14 11:11:59 +0900

    dd

--- python-3.7.8.sh
+++ python-3.7.8.sh
@@ -1,12 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.7
+sudo apt install python3.7 -y
 
 sudo apt update
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
 wget https://www.python.org/ftp/python/3.7.8/Python-3.7.8.tar.xz
 
 tar -xf Python-3.7.8.tar.xz
